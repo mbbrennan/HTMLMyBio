351da8f793793c56bef18c10a438f95aa60cc8f4
Maureen Brennan <mbbrennan@gmail.com>
2021-07-21 00:07:15 -0600

    Add files via upload
    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/*function SayHello(name)
+{
+    console.log('Hello ' + name)
+
+}
+
+SayHello('Louise Willis')
+*/
+//console.log(module);
+const logger = require('./logger.js');
+//logger = 1;
+//console.log(logger);
+logger('Louise Willis');
